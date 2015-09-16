db7b193c4028f8a387f61c7991f195fbce4d0dc8
Sidi Mohamed El Aatifi <elaatifi@gmail.com>
2015-09-16 19:28:06 +0200

    Using BoundedMapperFacade for Orika

--- src/main/java/com/javaetmoi/benchmark/mapping/mapper/orika/OrikaMapper.java
+++ src/main/java/com/javaetmoi/benchmark/mapping/mapper/orika/OrikaMapper.java
@@ -1,20 +1,29 @@
 package com.javaetmoi.benchmark.mapping.mapper.orika;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.javaetmoi.benchmark.mapping.mapper.OrderMapper;
 import com.javaetmoi.benchmark.mapping.model.dto.OrderDTO;
 import com.javaetmoi.benchmark.mapping.model.entity.Order;
+
+import ma.glasnost.orika.BoundMapperFacade;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
+import ma.glasnost.orika.impl.NonCyclicMappingContext;
 import ma.glasnost.orika.metadata.ClassMapBuilder;
 
 public class OrikaMapper implements OrderMapper {
 
     private MapperFacade facade;
+	private BoundMapperFacade<Order, OrderDTO> boundedMapper;
 
     public OrikaMapper() {
-        MapperFactory factory = new DefaultMapperFactory.Builder().build();
-        factory.registerClassMap(ClassMapBuilder.map(Order.class, OrderDTO.class)
+        MapperFactory factory = new DefaultMapperFactory.Builder()
+        .dumpStateOnException(false)
+        .mappingContextFactory(new NonCyclicMappingContext.Factory(new ConcurrentHashMap<Object, Object>()))
+        .build();
+        factory.classMap(Order.class, OrderDTO.class)
                 .field("customer.name", "customerName")
                 .field("customer.billingAddress.street",
                         "billingStreetAddress")
@@ -24,14 +33,14 @@ public class OrikaMapper implements OrderMapper {
                 .field("customer.shippingAddress.city",
                         "shippingCity")
                 .field("products", "products")
-                .toClassMap());
-        factory.build();
+                .register();
         facade = factory.getMapperFacade();
+        boundedMapper = factory.getMapperFacade(Order.class, OrderDTO.class);
     }
 
     @Override
     public OrderDTO map(Order source) {
-        return facade.map(source, OrderDTO.class);
+        return boundedMapper.map(source);
     }
 };
 
