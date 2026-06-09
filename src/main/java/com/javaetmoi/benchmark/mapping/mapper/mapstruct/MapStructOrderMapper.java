40111a6174cf10b325838e94c1bf65aa8dfca461
Antoine Rey <antoine.rey@free.fr>
2026-06-09 22:15:46 +0200

    Remove the unnecessary org.mapstruct.Mappings annotation

--- src/main/java/com/javaetmoi/benchmark/mapping/mapper/mapstruct/MapStructOrderMapper.java
+++ src/main/java/com/javaetmoi/benchmark/mapping/mapper/mapstruct/MapStructOrderMapper.java
@@ -7,18 +7,15 @@ import com.javaetmoi.benchmark.mapping.model.entity.Order;
 import com.javaetmoi.benchmark.mapping.model.entity.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper
 public interface MapStructOrderMapper extends OrderMapper {
 
-    @Mappings({
-            @Mapping(source = "customer.name", target = "customerName"),
-            @Mapping(source = "customer.billingAddress.street", target = "billingStreetAddress"),
-            @Mapping(source = "customer.billingAddress.city", target = "billingCity"),
-            @Mapping(source = "customer.shippingAddress.street", target = "shippingStreetAddress"),
-            @Mapping(source = "customer.shippingAddress.city", target = "shippingCity"),
-    })
+    @Mapping(source = "customer.name", target = "customerName")
+    @Mapping(source = "customer.billingAddress.street", target = "billingStreetAddress")
+    @Mapping(source = "customer.billingAddress.city", target = "billingCity")
+    @Mapping(source = "customer.shippingAddress.street", target = "shippingStreetAddress")
+    @Mapping(source = "customer.shippingAddress.city", target = "shippingCity")
     OrderDTO map(Order source);
 
     @Mapping(source = "name", target = "name")
